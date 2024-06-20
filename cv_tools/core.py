a1ab8aa3223d71794f3567a578cdbff8e5df6798
Goni Mohammed Hasan (BE TTI AET DEV DSW) <MohammedHasan.Goni@infineon.com>
2024-06-20 15:52:55 +0200

    {1}

--- cv_tools/core.py
+++ cv_tools/core.py
@@ -2,12 +2,12 @@
 
 # %% auto 0
 __all__ = ['OpenCvImage', 'get_name_', 'dpi', 'label_mask', 'show_labeled_mask', 'write_new_mask', 'remove_object_from_mask',
-           'read_img', 'blur_img_', 'median_img_', 'canny_img_', 'sharpen_img_', 'nlm_filter_', 'tv_filter_',
-           'filter_img_', 'show_', 'center_crop', 'overlay_mask', 'overlay_mask_border_on_image',
-           'overlay_mask_border_on_image_frm_img', 'concat_images', 'show_poster_from_path', 'seamless_clone',
-           'get_template_part', 'split_image', 'split_image_with_coordinates', 'create_same_shape',
-           'get_circle_from_single_pin', 'find_contours_binary', 'adjust_brightness', 'ssim_', 'orb_sim_',
-           'rot_based_on_ref_img', 'frm_cntr_to_bbox', 'foo']
+           'read_img', 'blur_img_', 'median_img_', 'canny_img_', 'sharpen_img_', 'bilateral_img_', 'nlm_filter_',
+           'tv_filter_', 'filter_img_', 'add_text_to_image', 'denoise_img', 'show_', 'center_crop', 'overlay_mask',
+           'overlay_mask_border_on_image', 'overlay_mask_border_on_image_frm_img', 'concat_images',
+           'show_poster_from_path', 'seamless_clone', 'get_template_part', 'split_image',
+           'split_image_with_coordinates', 'create_same_shape', 'get_circle_from_single_pin', 'find_contours_binary',
+           'adjust_brightness', 'ssim_', 'orb_sim_', 'rot_based_on_ref_img', 'frm_cntr_to_bbox', 'foo']
 
 # %% ../nbs/00_core.ipynb 3
 from PIL import Image
@@ -156,9 +156,18 @@ def sharpen_img_(
 
 
 # %% ../nbs/00_core.ipynb 17
+def bilateral_img_(
+        img:np.ndarray,  #image
+        d:int=9, # diameter of each pixel neighborhood
+        sc:int=75, # sigma color
+        ss:int=75, # sigma space
+        ) -> np.ndarray:
+    'Bilateral image'
+    return cv2.bilateralFilter(img, d, sc, ss)
+
+# %% ../nbs/00_core.ipynb 18
 def nlm_filter_(
     img:np.ndarray,  #image
-    multichannel:bool=False,
     h:float=1.15,
     fast_mode:bool=True,
     patch_size:int=5,
@@ -166,40 +175,39 @@ def nlm_filter_(
 
   ):
   'Apply Non local means denoising filter to image'
-  img = img_as_float(img)
+  img = img.astype(np.float)
   sigma_est = np.mean(
-    estimate_sigma(
-      img, multichannel=multichannel
-                  ) )
+    estimate_sigma(img) 
+    )
   denoise_img = denoise_nl_means(
     img, 
     h=h* sigma_est, 
     fast_mode=fast_mode,
     patch_size=patch_size,
     patch_distance=patch_distance,
-    multichannel=multichannel)
+    )
   return denoise_img
 
-# %% ../nbs/00_core.ipynb 18
+# %% ../nbs/00_core.ipynb 20
 def tv_filter_(
     img:np.ndarray,  #image
-    multichannel:bool=False,
     weight:float=0.1, # greater the weight, more denoising, but with less details
-    n_iter_max:int=200,
+    max_num_iter:int=200,
 
     ):
     'Apply total variation denoising filter to image'
-    img = img_as_float(img)
-    d_img = denoise_tv_chambolle(
-        img, 
-        weight=0.1, 
-        eps=0.0002,
-        n_iter_max=n_iter_max,
-        multichannel=multichannel)
-    return d_img
+    #img = img.astype(np.float)
+    #d_img = denoise_tv_chambolle(
+        #img, 
+        #weight=0.1, 
+        #eps=0.0002,
+        #max_num_iter=max_num_iter,
+        #)
+    dst = cv2.fastNlMeansDenoising(np.clip(img,0,255), None, 4, 7, 35)
+    return dst
     
 
-# %% ../nbs/00_core.ipynb 19
+# %% ../nbs/00_core.ipynb 22
 def filter_img_(
         img:np.ndarray,  #image
         filters_:List[str] # opencv filters
@@ -214,6 +222,7 @@ def filter_img_(
         'sharpen': sharpen_img_,
         'nlm': nlm_filter_,
         'tv': tv_filter_,
+        'bilateral': bilateral_img_,
     }
     for fl in filters_:
         if fl in ops:
@@ -222,7 +231,71 @@ def filter_img_(
             print(f'Filter {fl} not found')
     return img
 
-# %% ../nbs/00_core.ipynb 20
+# %% ../nbs/00_core.ipynb 23
+def add_text_to_image(
+                        img: np.ndarray, 
+                        text: str, 
+                        position: Tuple[int, int]=None,
+                        font=cv2.FONT_HERSHEY_SIMPLEX, 
+                        font_scale: float=1, 
+                        color: Tuple[int, int, int]=(255, 255, 255), 
+                        thickness: int=2):
+    """
+    Add text to an image.
+
+    Parameters:
+    img (np.ndarray): The image to add text to.
+    text (str): The text to add.
+    position (Tuple[int, int]): The position where the text should be added.
+    font: The font to use. Default is cv2.FONT_HERSHEY_SIMPLEX.
+    font_scale (float): The font scale. Default is 1.
+    color (Tuple[int, int, int]): The color of the text in BGR format. Default is white.
+    thickness (int): The thickness of the text. Default is 2.
+
+    Returns:
+    np.ndarray: The image with the text added.
+    """
+    if position is None:
+        # Set the position to a small offset from the top left corner
+        position = (10, 30)  # 10 pixels from the left, 30 pixels from the top
+    img_with_text = cv2.putText(img.copy(), text, position, font, font_scale, color, thickness)
+    return img_with_text
+
+# %% ../nbs/00_core.ipynb 24
+def denoise_img(
+    img:np.ndarray,  #image
+    filters_:List[str], # opencv filters
+    h:float=1.15,
+    fast_mode:bool=True,
+    patch_size:int=5,
+    patch_distance:int=3,
+    multichannel:bool=False,
+    weight:float=0.1, # greater the weight, more denoising, but with less details
+    n_iter_max:int=200,
+    ):
+    'Apply filter on image'
+
+    ops = {
+        'blur': blur_img_,
+        'med': median_img_,
+        'canny': canny_img_,
+        'sharpen': sharpen_img_,
+        'nlm': nlm_filter_,
+        'tv': tv_filter_,
+        'bilateral': bilateral_img_,
+    }
+    images_= []
+    for fl in filters_:
+        if fl in ops:
+            img_ = ops[fl](img)
+            image_ = add_text_to_image(img_, fl)
+            images_.append(image_)
+
+        else:
+            print(f'Filter {fl} not found')
+    return show_(np.concatenate(images_, axis=1))
+
+# %% ../nbs/00_core.ipynb 26
 def show_(
     im_path:Union[str, np.ndarray],
 	cmap:str='gray'
@@ -256,7 +329,7 @@ def show_(
         ax.imshow(im,cmap=cmap)
         ax.axis('off')
 
-# %% ../nbs/00_core.ipynb 21
+# %% ../nbs/00_core.ipynb 27
 #def normalize(
               #image:Union[np.ndarray, tf.Tensor], 
               #min=0):
@@ -269,7 +342,7 @@ def show_(
     #else:
         #return (_normalize(image) * 2.0) -1.0
 
-# %% ../nbs/00_core.ipynb 22
+# %% ../nbs/00_core.ipynb 28
 def center_crop(
         image:Image,# not open cv but PIL Image 
         desired_width:int=1632,
@@ -301,7 +374,7 @@ def center_crop(
 
     return cropped_image
 
-# %% ../nbs/00_core.ipynb 23
+# %% ../nbs/00_core.ipynb 29
 def overlay_mask(
         im_path:Union[str,Path],
         msk_path:Union[str,Path], 
@@ -334,7 +407,7 @@ def overlay_mask(
     clrd_overlay[mask_img]=overlay_clr
     ax.imshow(clrd_overlay, alpha=alpha)
 
-# %% ../nbs/00_core.ipynb 24
+# %% ../nbs/00_core.ipynb 30
 def overlay_mask_border_on_image(
         im_path: Union[Path, str],
         msk_path: Union[Path, str],
@@ -405,7 +478,7 @@ def overlay_mask_border_on_image(
 
 
 
-# %% ../nbs/00_core.ipynb 25
+# %% ../nbs/00_core.ipynb 31
 def overlay_mask_border_on_image_frm_img(
         img:Union[OpenCvImage, Image.Image],
         msk:Union[OpenCvImage, Image.Image],
@@ -429,12 +502,14 @@ def overlay_mask_border_on_image_frm_img(
     return cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
 
 
-# %% ../nbs/00_core.ipynb 26
+# %% ../nbs/00_core.ipynb 32
 def concat_images(
         images:List[np.ndarray],
         rows:int,  # number of rows
         cols:int,  # number of columns in combined images
-        number:str # a text which will be inserted in the combined image
+        number:str, # a text which will be inserted in the combined image
+        border_size:int=10, # border size
+        border_color:Tuple[int, int, int]=(0, 0, 0) # border color
         ):
     'Concate images rows and cols and add a number to the image.'
     targe_h = min([i.shape[0] for i in images])
@@ -445,7 +520,9 @@ def concat_images(
         start_index = i * cols
         end_index = start_index + cols
         row_images = res_img[start_index:end_index]
+        row_images = [cv2.copyMakeBorder(img, 0, 0, 0, border_size, cv2.BORDER_CONSTANT, value=border_color) for img in row_images]
         row_concat = cv2.hconcat(row_images)
+
         res.append(row_concat)
     new_img = cv2.vconcat(res)
     position = (6, 25)
@@ -455,7 +532,7 @@ def concat_images(
     im_n = cv2.putText(new_img, f'{number}', position, font, font_scale, color, 1, cv2.LINE_AA)
     return im_n
 
-# %% ../nbs/00_core.ipynb 27
+# %% ../nbs/00_core.ipynb 33
 def show_poster_from_path(
                 mask_path:str, 
                 im_path:str, # name of the image fodler , e.g. 'images' or 'X'
@@ -505,7 +582,7 @@ def show_poster_from_path(
         fig, ax = plt.subplots(1, 1, figsize=(scale * res[1]/dpi, scale*res[0]/dpi))
         ax.imshow(img_new)
 
-# %% ../nbs/00_core.ipynb 28
+# %% ../nbs/00_core.ipynb 34
 def seamless_clone(
         full_img:OpenCvImage, # gray scale image
         replace_part:OpenCvImage, # gray scale image
@@ -534,7 +611,7 @@ def seamless_clone(
     
     return cv2.cvtColor(cloned_img, cv2.COLOR_BGR2GRAY)
 
-# %% ../nbs/00_core.ipynb 29
+# %% ../nbs/00_core.ipynb 35
 def get_template_part(
     img:np.ndarray, #opencv image
     tmp_img:np.ndarray # opencv image
@@ -559,7 +636,7 @@ def get_template_part(
    return x, y, w, h
    
 
-# %% ../nbs/00_core.ipynb 30
+# %% ../nbs/00_core.ipynb 36
 def split_image(
         img:np.ndarray,
         num_splits:int,
@@ -590,7 +667,7 @@ def split_image(
 
     return parts
 
-# %% ../nbs/00_core.ipynb 31
+# %% ../nbs/00_core.ipynb 37
 def split_image_with_coordinates(
         img: np.ndarray, 
         num_splits: int, 
@@ -614,7 +691,7 @@ def split_image_with_coordinates(
 
     return indexed_parts_with_coords
 
-# %% ../nbs/00_core.ipynb 32
+# %% ../nbs/00_core.ipynb 38
 def create_same_shape(
         src_img:np.ndarray, # image which size needs to be replicated
         dst_img:np.ndarray, # image which needs to resized
@@ -633,7 +710,7 @@ def create_same_shape(
         return new
 
 
-# %% ../nbs/00_core.ipynb 33
+# %% ../nbs/00_core.ipynb 39
 def get_circle_from_single_pin(
                             sn_pin_img:np.ndarray
                               )->Tuple[np.ndarray, np.ndarray, np.ndarray]:
@@ -678,7 +755,7 @@ def get_circle_from_single_pin(
         return None, None, None
 
 
-# %% ../nbs/00_core.ipynb 34
+# %% ../nbs/00_core.ipynb 40
 def find_contours_binary(
     img:np.ndarray, # binary image 
     ):
@@ -687,7 +764,7 @@ def find_contours_binary(
     return cntrs
 
 
-# %% ../nbs/00_core.ipynb 35
+# %% ../nbs/00_core.ipynb 41
 def adjust_brightness(
     img:np.ndarray, # image to adjust brightness
     alpha:float, # alpha > 1 to brighten; alpha < 1 to darken
@@ -698,7 +775,7 @@ def adjust_brightness(
         alpha=alpha)  # alpha > 1 to brighten; alpha < 1 to darken
     return adjusted
 
-# %% ../nbs/00_core.ipynb 36
+# %% ../nbs/00_core.ipynb 42
 def ssim_(
         img1:np.ndarray, 
         img2:np.ndarray,
@@ -708,7 +785,7 @@ def ssim_(
     return ssim(img1, img2, win_size=win_size
                 )
 
-# %% ../nbs/00_core.ipynb 37
+# %% ../nbs/00_core.ipynb 43
 def orb_sim_(
         img1:np.ndarray, 
         img2:np.ndarray):
@@ -733,7 +810,7 @@ def orb_sim_(
         return len(list(sim_reg))/len(matches)
 
 
-# %% ../nbs/00_core.ipynb 38
+# %% ../nbs/00_core.ipynb 44
 def rot_based_on_ref_img(
     ref_img:Union[np.ndarray, Image.Image], # Image will be used refeernece image
     tst_img:Union[np.ndarray, Image.Image], # Image where searched for key points
@@ -774,10 +851,10 @@ def rot_based_on_ref_img(
     
 
 
-# %% ../nbs/00_core.ipynb 39
+# %% ../nbs/00_core.ipynb 45
 def frm_cntr_to_bbox(cntr):
     x,y,w,h = cv2.boundingRect(cntr)
     return x,y,w,h
 
-# %% ../nbs/00_core.ipynb 40
+# %% ../nbs/00_core.ipynb 46
 def foo(): pass
